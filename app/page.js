e304cc61b43569ee1cf4f20d88caa3d77df0e2da
Abdelbasset Meghraoui <124573314+abdelbasset-mg@users.noreply.github.com>
2023-11-09 00:24:37 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -32,9 +32,7 @@ export default function Home() {
               <h1 className="font-bold text-5xl lg:text-6xl mb-4 max-w-2xl lg:leading-[106px]">
                 Survision
                 <br></br>
-                Clear Data
-                <br></br>
-                Clear Vision
+                Clear Data Clear Vision
               </h1>
               <p className="opacity-90 max-w-2xl  lg:text-2xl font-medium">
                 At Survision, we are changing the game in market research.
