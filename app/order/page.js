e6c60ea331b995543650ec1f8b4d7a2f065db06a
Abdelbasset Meghraoui <124573314+abdelbasset-mg@users.noreply.github.com>
2023-12-04 20:54:50 +0100

    Update page.js

--- app/order/page.js
+++ app/order/page.js
@@ -18,7 +18,7 @@ function page() {
           </p>
           <p className="mb-2 lg:mb-4">
             {" "}
-            0782277055 <br></br> survisiondz@gmail.com <br></br> N32 Les Pain
+            0541868191 <br></br> survision@gmail.com <br></br> N32 Les Pain
             Marytims, Alger{" "}
           </p>
         </div>
