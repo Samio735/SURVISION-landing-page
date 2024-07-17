084757fa0f7d9242877b8ebacce94b20e4f3c77b
Samy Rahim <102865808+Samio735@users.noreply.github.com>
2024-07-17 15:41:43 +0100

    Update page.js

--- app/app/page.js
+++ app/app/page.js
@@ -22,7 +22,7 @@ function page() {
           <p className="mb-4 lg:mb-8">
             3. Start Collecting: Use the Survision app to gather data from surveys in your area.
           </p>
-          <button className="flex bg-backgroundWhite text-darkBlue items-center gap-2 py-2 px-4 rounded-lg " onClick={() => window.location.href='https://docs.google.com/forms/d/1two7Wc0Lm6pRdU_QQO7BWDKVVXb-wvPGxPfHehmKQa0/prefill'}>
+          <button className="flex bg-backgroundWhite text-darkBlue items-center gap-2 py-2 px-4 rounded-lg " onclick="window.location.href='https://docs.google.com/forms/d/1two7Wc0Lm6pRdU_QQO7BWDKVVXb-wvPGxPfHehmKQa0/prefill';">
             
             <p>Application Form</p>
           </button>
